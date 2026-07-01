e758aed6a46908dc7051bf34642528b6454c6e22
Whatstone <whatston3@gmail.com>
2026-07-01 00:50:36 -0400

    TryGetRandomTile: use iterators

--- Content.Server/GameTicking/Rules/GameRuleSystem.Utility.cs
+++ Content.Server/GameTicking/Rules/GameRuleSystem.Utility.cs
@@ -1,18 +1,15 @@
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using Content.Server.Station.Components;
 using Content.Shared.GameTicking.Components;
-using Content.Shared.Random.Helpers;
 using Content.Shared.Station.Components;
 using Robust.Shared.Collections;
 using Robust.Shared.Map;
 using Robust.Shared.Map.Components;
-using Robust.Shared.Utility;
+using Robust.Shared.Map.Enumerators;
 
 namespace Content.Server.GameTicking.Rules;
 
-public abstract partial class GameRuleSystem<T> where T: IComponent
+public abstract partial class GameRuleSystem<T> where T : IComponent
 {
     protected EntityQueryEnumerator<ActiveGameRuleComponent, T, GameRuleComponent> QueryActiveRules()
     {
@@ -81,99 +78,145 @@ public abstract partial class GameRuleSystem<T> where T: IComponent
         return false;
     }
 
+    /// <summary>
+    /// Finds an internal tile within a station.
+    /// The tile found must have a valid atmosphere to return true.
+    /// Has a (roughly) uniform probability per tile.
+    /// </summary>
+    /// <param name="station">The station to search.</param>
+    /// <param name="tile">The grid index of the tile found, or Vector2i.Zero if none was found.</param>
+    /// <param name="targetGrid">The grid the tile found was on, or EntityUid.Invalid if none was found</param>
+    /// <param name="targetCoords">The coordinates relative to the grid of the center of the tile found, or Invalid if none was found.</param>
+    /// <returns>true if a valid internal tile was found, false otherwise.</returns>
     protected bool TryFindRandomTileOnStation(Entity<StationDataComponent> station,
         out Vector2i tile,
         out EntityUid targetGrid,
-        out EntityCoordinates targetCoords,
-        int numAttempts = 10)
+        out EntityCoordinates targetCoords)
     {
         tile = default;
         targetCoords = EntityCoordinates.Invalid;
         targetGrid = EntityUid.Invalid;
 
-        // Weight grid choice by tilecount
+        // Weight grid choice by tile count.
         var totalTiles = 0;
-        var grids = new List<(Entity<MapGridComponent> Entity, int Count, List<TileRef> Tiles)>();
+        var grids = new List<(Entity<MapGridComponent> Grid, int Count)>();
         foreach (var possibleTarget in station.Comp.Grids)
         {
-            if (!TryComp<MapGridComponent>(possibleTarget, out var comp))
+            if (!TryComp<MapGridComponent>(possibleTarget, out var gridComp))
                 continue;
 
-            // Get the tile count for the given grid.
-            var tileCount = _map.GetFilledTileCount((possibleTarget, comp));
+            var tileCount = _map.GetFilledTileCount((possibleTarget, gridComp));
 
-            // Just to be sure, no empty elements.
+            // No empty elements.
             if (tileCount > 0)
             {
-                grids.Add(((possibleTarget, comp), tileCount, new()));
+                grids.Add(((possibleTarget, gridComp), tileCount));
                 totalTiles += tileCount;
             }
         }
 
+        // No station grids have tiles, nothing to do.
         if (grids.Count == 0)
-        {
-            targetGrid = EntityUid.Invalid;
             return false;
-        }
 
-        for (var i = 0; i < numAttempts; i++)
+        // Pick a random tile index, iterate over tiles by that.
+        var startTileIndex = RobustRandom.Next(totalTiles);
+        var currentTileCount = 0;
+        var startGridIndex = -1;
+        for (var i = 0; i < grids.Count; i++)
         {
-            // Find random tile within list.
-            var nextTileIndex = RobustRandom.Next(totalTiles);
-            TileRef? randomTileRef = null;
-            MapGridComponent gridComp = default!;
-            var startIndex = 0;
-            for (int j = 0; j < grids.Count; j++)
+            if (startTileIndex < currentTileCount + grids[i].Count)
             {
-                var grid = grids[j];
-                // If the index is in this particular grid, find it and remove the tile to prevent selecting it twice.
-                if (nextTileIndex >= startIndex + grid.Count)
-                {
-                    startIndex += grid.Count;
-                    continue;
-                }
-
-                (targetGrid, gridComp) = grid.Entity;
-
-                // Empty list: hasn't been queried yet - get our tiles.
-                if (grid.Tiles.Count <= 0)
-                {
-                    grid.Tiles = _map.GetAllTiles(targetGrid, gridComp).ToList();
-
-                    // Actual list count doesn't match expected count (a bug - return failure).
-                    Debug.Assert(grid.Tiles.Count == grid.Count);
-                    if (grid.Tiles.Count != grid.Count)
-                        return false;
-                }
-
-                var ourTileIndex = nextTileIndex - startIndex;
-                randomTileRef = grid.Tiles[ourTileIndex];
-                grid.Tiles.RemoveSwap(ourTileIndex);
-                grid.Count--;
-                totalTiles--;
-
-                // Empty list, remove element
-                if (grid.Tiles.Count <= 0)
-                    grids.RemoveSwap(j);
-
+                startGridIndex = i;
+                startTileIndex -= currentTileCount; // Change from total tile index to grid-wide tile index.
                 break;
             }
+            currentTileCount += grids[i].Count;
+        }
 
-            // Out of valid tiles, return early.
-            if (randomTileRef is not { } tileRef)
-                return false;
+        // Check tiles in starting grid beginning with startTileIndex.
+        var tileEnumerator = _map.GetAllTilesEnumerator(grids[startGridIndex].Grid, grids[startGridIndex].Grid.Comp);
+        for (var i = 0; i < startTileIndex; i++)
+            tileEnumerator.MoveNext(out _);
 
-            // Invalid tile, try again.
-            if (_atmosphere.IsTileSpace(targetGrid, Transform(targetGrid).MapUid, tileRef.GridIndices)
-                || _atmosphere.IsTileAirBlockedCached(targetGrid, tile))
-            {
-                continue;
-            }
-
-            targetCoords = _map.GridTileToLocal(targetGrid, gridComp, tile);
+        if (CheckTilesInIterator(tileEnumerator, grids[startGridIndex].Grid, out var tileRef))
+        {
+            tile = tileRef.GridIndices;
+            targetGrid = grids[startGridIndex].Grid;
+            targetCoords = _map.GridTileToLocal(grids[startGridIndex].Grid, grids[startGridIndex].Grid.Comp, tile);
             return true;
         }
 
+        // Check everything after the starting grid.
+        for (var i = startGridIndex + 1; i < grids.Count; i++)
+        {
+            tileEnumerator = _map.GetAllTilesEnumerator(grids[startGridIndex].Grid, grids[startGridIndex].Grid.Comp);
+            if (CheckTilesInIterator(tileEnumerator, grids[startGridIndex].Grid, out tileRef))
+            {
+                tile = tileRef.GridIndices;
+                targetGrid = grids[startGridIndex].Grid;
+                targetCoords = _map.GridTileToLocal(grids[startGridIndex].Grid, grids[startGridIndex].Grid.Comp, tile);
+                return true;
+            }
+        }
+
+        // Check everything from the beginning of the list up to the starting grid.
+        for (var i = 0; i < startGridIndex; i++)
+        {
+            tileEnumerator = _map.GetAllTilesEnumerator(grids[startGridIndex].Grid, grids[startGridIndex].Grid.Comp);
+            if (CheckTilesInIterator(tileEnumerator, grids[startGridIndex].Grid, out tileRef))
+            {
+                tile = tileRef.GridIndices;
+                targetGrid = grids[startGridIndex].Grid;
+                targetCoords = _map.GridTileToLocal(grids[startGridIndex].Grid, grids[startGridIndex].Grid.Comp, tile);
+                return true;
+            }
+        }
+
+        // Check the tiles in the starting grid up to the start tile index.
+        tileEnumerator = _map.GetAllTilesEnumerator(grids[startGridIndex].Grid, grids[startGridIndex].Grid.Comp);
+        for (var i = 0; i < startTileIndex; i++)
+        {
+            if (!tileEnumerator.MoveNext(out var maybeTileRef) || maybeTileRef is not { } ourTileRef)
+                break;
+
+            if (IsValidTile(grids[startGridIndex].Grid, ourTileRef))
+            {
+                tile = ourTileRef.GridIndices;
+                targetGrid = grids[startGridIndex].Grid;
+                targetCoords = _map.GridTileToLocal(grids[startGridIndex].Grid, grids[startGridIndex].Grid.Comp, tile);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Returns if the given tile on the given grid is considered a valid internal tile.
+    /// </summary>
+    private bool IsValidTile(EntityUid targetGrid, TileRef tileRef)
+    {
+        return _atmosphere.IsTileSpace(targetGrid, null, tileRef.GridIndices) &&
+            _atmosphere.IsTileAirBlockedCached(targetGrid, tileRef.GridIndices);
+    }
+
+    /// <summary>
+    /// Checks if all tiles until the end of the given GridTileEnumerator are valid for placement,
+    /// returns the coordinates of the first one in outTile.
+    /// </summary>
+    private bool CheckTilesInIterator(GridTileEnumerator tileEnum, Entity<MapGridComponent> grid, [NotNullWhen(true)] out TileRef outTile)
+    {
+        while (tileEnum.MoveNext(out var maybeTileRef) && maybeTileRef is { } tileRef)
+        {
+            if (IsValidTile(grid, tileRef))
+            {
+                outTile = tileRef;
+                return true;
+            }
+        }
+
+        outTile = default;
         return false;
     }
 
